f15762d55b576e0ec251fa245b12daf0000ba6a3
akeredchieva <a.keredchieva@gmail.com>
2018-06-20 11:01:48 +0300

    Adding needed things in pom.xml. Add documantation for Application class.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,6 +6,9 @@ public class Application {
 
     public static void main(String[] args) throws IOException {
 
+        /**
+         * Create new GameEngine and start the game.
+         */
         GameEngine gameEngine = new GameEngine();
 
         gameEngine.start();
